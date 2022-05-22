a907cae9fb8a1fce9cc32ebe5abee09e1448e879
Rupanshu Gupta <72185267+rupanshu1710@users.noreply.github.com>
2022-05-22 15:48:08 +0530

    Added solution - LeetHub

--- 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
@@ -8,18 +8,28 @@ class Solution
 {
     public:
     //Function to return max value that can be put in knapsack of capacity W.
-    int t[1001][1001];
-    int knap(int w, int wt[], int val[], int n) 
-    { 
-       if(n==0  || w==0)return 0;
-       if(t[n][w]!=-1)return t[n][w];
-       if(wt[n-1]<=w)return t[n][w] = max(val[n-1]+knap(w-wt[n-1],wt,val,n-1),knap(w,wt,val,n-1));
-       else return t[n][w] = knap(w,wt,val,n-1);
-    }
     int knapSack(int w, int wt[], int val[], int n) 
-    {
-        memset(t,-1,sizeof(t));
-        knap(w,wt,val,n);
+    { 
+       int t[n+1][w+1];
+       for(int i=0; i<n+1; i++)
+       {
+           for(int j=0; j<w+1; j++)
+           {
+               if(i==0 || j==0)t[i][j] = 0;
+           }
+       }
+       for(int i=1; i<n+1; i++)
+       {
+           for(int j=1; j<w+1; j++)
+           {
+               if(wt[i-1]<=j)
+               {
+                   t[i][j] = max(val[i-1] + t[i-1][j-wt[i-1]],t[i-1][j]);
+               }
+               else t[i][j] = t[i-1][j];
+           }
+       }
+       return t[n][w];                                      
     }
 };
 
