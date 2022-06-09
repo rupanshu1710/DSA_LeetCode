af85184936c994495ab6f6223aa1476b6a2a98fc
Rupanshu Gupta <72185267+rupanshu1710@users.noreply.github.com>
2022-06-09 12:37:08 +0530

    Time: 144 ms (40.06%), Space: 10.6 MB (84.28%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -3,29 +3,22 @@ public:
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
         int t[n+1][amount+1];
-        for(int i=0; i<n+1; i++)
-        {
-            for(int j=0; j<amount+1; j++)
-            {
-                if(i==0||j==0)t[i][j]=0;
-                if(i==0)t[i][j]=INT_MAX-1;
-                if(i==1 && j!=0)
-                {
-                    if(j%coins[0]==0)t[i][j]=j/coins[0];
-                    else t[i][j]=INT_MAX-1;
+        for(int i=0; i<n+1; i++){
+            for(int j=0; j<amount+1; j++){
+                if(!j)t[i][j] = 0;
+                if(!i)t[i][j] = INT_MAX-1;
+                if(i==1 && j!=0){
+                    if(j%coins[0]!=0)t[i][j] = INT_MAX-1;
+                    else t[i][j] = j/coins[0];
                 }
             }
         }
-        for(int i=2; i<=n; i++)
-        {
-            for(int j=1; j<=amount; j++)
-            {
-                if(coins[i-1]<=j)
-                {
-                    t[i][j] = min(t[i][j-coins[i-1]]+1, t[i-1][j]);
+        for(int i=2; i<n+1; i++){
+            for(int j=1; j<amount+1; j++){
+                if(coins[i-1]<=j){
+                    t[i][j] = min(1+t[i][j-coins[i-1]],t[i-1][j]);
                 }
-                else
-                {
+                else{
                     t[i][j] = t[i-1][j];
                 }
             }
