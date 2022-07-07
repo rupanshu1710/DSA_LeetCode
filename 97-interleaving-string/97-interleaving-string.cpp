a31caf5b376f4d9cfb74db128bbd8e56c195c9d0
Rupanshu Gupta <72185267+rupanshu1710@users.noreply.github.com>
2022-07-07 15:31:14 +0530

    Time: 183 ms (5.02%), Space: 40.1 MB (8.43%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -1,24 +1,18 @@
 class Solution {
 public:
+    unordered_map<string,bool> mp;
+    bool getAns(string s1, string s2, string s3, int i, int j, int k){
+        if(k==s3.size()) return (i==s1.size() and j==s2.size())?1:0;
+        string temp = to_string(i) + "*" + to_string(j) + "*" + to_string(k);
+        if(mp.find(temp)!=mp.end())return mp[temp];
+        if(i==s1.size()) return s2[j]==s3[k]?getAns(s1,s2,s3,i,j+1,k+1):0;
+        if(j==s2.size()) return s1[i]==s3[k]?getAns(s1,s2,s3,i+1,j,k+1):0;
+        bool c1 = 0,c2=0;
+        if(s1[i]==s3[k]) c1 =  getAns(s1,s2,s3,i+1,j,k+1);
+        if(s2[j]==s3[k]) c2 =  getAns(s1,s2,s3,i,j+1,k+1);
+        return mp[temp] = c1 or c2;
+    }
     bool isInterleave(string s1, string s2, string s3) {
-    
-    if(s3.length() != s1.length() + s2.length())
-        return false;
-    
-    bool table[s1.length()+1][s2.length()+1];
-    
-    for(int i=0; i<s1.length()+1; i++)
-        for(int j=0; j< s2.length()+1; j++){
-            if(i==0 && j==0)
-                table[i][j] = true;
-            else if(i == 0)
-                table[i][j] = ( table[i][j-1] && s2[j-1] == s3[i+j-1]);
-            else if(j == 0)
-                table[i][j] = ( table[i-1][j] && s1[i-1] == s3[i+j-1]);
-            else
-                table[i][j] = (table[i-1][j] && s1[i-1] == s3[i+j-1] ) || (table[i][j-1] && s2[j-1] == s3[i+j-1] );
-        }
-        
-    return table[s1.length()][s2.length()];
-}
+        return getAns(s1,s2,s3,0,0,0);
+    }
 };
