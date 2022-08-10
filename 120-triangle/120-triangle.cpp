7e225bab3f6f9699bf8aa121ff0af2a7d82f8257
Rupanshu Gupta <72185267+rupanshu1710@users.noreply.github.com>
2022-08-10 13:05:42 +0530

    Time: 8 ms (67.81%), Space: 8.7 MB (50.36%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     int t[201][201];
-    int getAns(vector<vector<int>>& triangle,int i,int j){
-        if(i==triangle.size()-1)return triangle[i][j];
+    int getAns(vector<vector<int>>& triangle, int i, int j){
+        if(i>triangle.size()-1)return 0;
         if(t[i][j]!=-1)return t[i][j];
-        return  t[i][j] = triangle[i][j] + min(getAns(triangle,i+1,j),getAns(triangle,i+1,j+1));
+        return t[i][j] = triangle[i][j] + min(getAns(triangle,i+1,j+1),getAns(triangle,i+1,j));
     }
     int minimumTotal(vector<vector<int>>& triangle) {
         memset(t,-1,sizeof(t));
