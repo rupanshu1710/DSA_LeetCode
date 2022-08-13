2fe5e06bc058ac151f253dcd5d6bd6d5a02ecb6b
Rupanshu Gupta <72185267+rupanshu1710@users.noreply.github.com>
2022-08-13 23:28:57 +0530

    Time: 11 ms (10.70%), Space: 7.4 MB (22.24%) - LeetHub

---
+++ 91-decode-ways/91-decode-ways.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int t[101];
+    int getAns(string s, int idx, int n){
+        if(idx==n) return 1;
+        if(s[idx]=='0') return 0;
+        if(t[idx]!=-1)return t[idx];
+        string s1 = s.substr(idx, 2);
+        int ans1 = getAns(s,idx+1,n);
+        int ans2 = 0;
+        if(stoi(s1)>9 and stoi(s1)<=26)ans2 = getAns(s, idx+2, n);
+        return t[idx] = ans1+ans2;        
+        
+    }
+    int numDecodings(string s) {
+        int n=s.size();
+        for(int i=0; i<101; i++)t[i]=-1;
+        return getAns(s,0,n);
+    }
+};
