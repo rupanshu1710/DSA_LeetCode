e3dd37aeb972026e785aab452d85d6ea1bf19584
Rupanshu Gupta <72185267+rupanshu1710@users.noreply.github.com>
2022-08-13 12:47:53 +0530

    Time: 47 ms (68.77%), Space: 18.4 MB (10.82%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,12 +1,26 @@
 class Solution {
 public:
-    void dfs(int i, int j,vector<vector<char>>& grid, vector<vector<bool>>& visited ){
-        if(i<0 or j<0 or i>=grid.size() or j>=grid[0].size() or visited[i][j] or grid[i][j]=='0')return;
+    void bfs(int i, int j,vector<vector<char>>& grid, vector<vector<bool>>& visited ){
         visited[i][j] = 1;
-        dfs(i+1,j,grid,visited);
-        dfs(i-1,j,grid,visited);
-        dfs(i,j+1,grid,visited); 
-        dfs(i,j-1,grid,visited);
+        queue<pair<int,int>> q;
+        q.push({i,j});
+        int m = grid.size();
+        int n = grid[0].size();
+        while(!q.empty()){
+            int i = q.front().first;
+            int j = q.front().second;
+            q.pop();
+            int dx[4] = {-1, 0, 1, 0};
+            int dy[4] = {0, 1, 0, -1};
+            for(int a=0; a<4; a++){
+                int na = i+dx[a];
+                int nb = j +dy[a];
+                if(na>=0 and na<m  and  nb>=0 and nb<n and grid[na][nb]=='1' and !visited[na][nb]){
+                    visited[na][nb]=1;
+                    q.push({na,nb});
+                }
+            }
+        }
     }
     int numIslands(vector<vector<char>>& grid) {
         int m = grid.size();
@@ -17,7 +31,7 @@ public:
             for(int j=0; j<n; j++){
                 if(grid[i][j]=='1' && !vis[i][j]){
                     ans++;
-                    dfs(i,j,grid,vis);
+                    bfs(i,j,grid,vis);
                 }
             }
         }
