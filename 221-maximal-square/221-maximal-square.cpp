7ac025f08545ccca82fd21eface4f377e3b237b3
Rupanshu Gupta <72185267+rupanshu1710@users.noreply.github.com>
2022-08-15 23:10:47 +0530

    Time: 77 ms (82.13%), Space: 17.3 MB (74.32%) - LeetHub

--- 221-maximal-square/221-maximal-square.cpp
+++ 221-maximal-square/221-maximal-square.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
-    int t[301][301]; 
-    int getAns(vector<vector<char>>& matrix,int i, int j,int& mx){
-        if(i>=matrix.size() || j>=matrix[0].size())return 0;
-        if(t[i][j]!=-1)return t[i][j];
-        int right = getAns(matrix,i,j+1,mx);
-        int diag = getAns(matrix,i+1,j+1,mx);
-        int down = getAns(matrix,i+1, j,mx);
-        if(matrix[i][j]=='1'){
-            int ans = 1 + min(right,min(diag,down));
-            mx = max(mx,ans);
-            return t[i][j] = ans;
-        }
-        else return t[i][j] = 0;
-    }
     int maximalSquare(vector<vector<char>>matrix) {
         int mx=0;
-        memset(t,-1,sizeof(t));
-        getAns(matrix,0,0,mx);
+        int n = matrix.size();
+        int m = matrix[0].size();
+        int t[n+1][m+1];
+        memset(t,0,sizeof(t));
+        for(int i=n-1; i>=0; i--){
+            for(int j=m-1; j>=0; j--){
+                int right = t[i][j+1];
+                int diag = t[i+1][j+1];
+                int down = t[i+1][j];
+                if(matrix[i][j]=='1'){
+                    t[i][j] = 1 + min(right,min(diag,down));
+                    mx = max(mx,t[i][j]);
+                }
+                else t[i][j] = 0;
+            }
+        }
         return mx*mx;
     }
 };
