f2ddf4c84e56ed01798dc2cd6aa8497d8df2540e
Rupanshu Gupta <72185267+rupanshu1710@users.noreply.github.com>
2023-01-03 17:14:26 +0530

    Added solution - LeetHub

---
+++ First negative integer in every window of size k - GFG/first-negative-integer-in-every-window-of-size-k.cpp
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<long long> printFirstNegativeInteger(long long int arr[],
+                                             long long int n, long long int k);
+
+// Driver program to test above functions
+int main() {
+    long long int t, i;
+    cin >> t;
+    while (t--) {
+        long long int n;
+        cin >> n;
+        long long int arr[n];
+        for (i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        long long int k;
+        cin >> k;
+
+        vector<long long> ans = printFirstNegativeInteger(arr, n, k);
+        for (auto it : ans) cout << it << " ";
+        cout << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
+
+
+vector<long long> printFirstNegativeInteger(long long int a[], long long int n, long long int k) {
+    vector<long long> ans;
+    int temp=0;
+    queue<long long>q;
+    for(int i=0; i<k; i++){
+        if(a[i]<0)q.push(a[i]);
+    }
+    if(q.size()>0)ans.push_back(q.front());
+    else ans.push_back(0);
+    for(int i=k; i<n; i++){
+        if(a[i]<0){
+            q.push(a[i]);
+        }
+        if(a[i-k]<0 && q.size()>0){
+            q.pop();
+        }
+        if(q.size()>0){
+            ans.push_back(q.front());
+        }
+        else{
+            ans.push_back(0);
+        }
+    }
+    return ans;
+ }
