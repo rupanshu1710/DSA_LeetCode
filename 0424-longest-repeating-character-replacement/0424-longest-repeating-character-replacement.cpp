c59382fd7ac7a8dea16994298a28f3203dd54450
Rupanshu Gupta <72185267+rupanshu1710@users.noreply.github.com>
2023-01-14 23:50:56 +0530

    Time: 728 ms (5.02%), Space: 16.2 MB (5.20%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -0,0 +1,62 @@
+class Solution {
+public:
+    bool isCheck(string s, int len, int k){
+        if(len<=k)return 1;
+        int i=0,j=0,ans=0;
+        vector<int>mp(26,0);
+        int count=0;
+        int cnt=0,mx;
+        while(j<s.size()){
+            int sum=0;
+            mp[s[j]-'A']++;
+            if(s[i]!=s[j])count++;
+            mx = *max_element(mp.begin(),mp.end());
+            for(int i=0; i<26; i++)sum += mp[i];
+            sum -= mx;
+            // if(i<s.size()-1 && s[i+1]!=s[j])cnt++;
+            if(sum<=k){
+                ans = max(ans,j-i+1);
+                // cout<<i<<" "<<j<<" "<<count<<endl;
+                if(ans>=len)return 1;
+                j++;
+            }
+            else{
+                char c = s[i];
+                int x=0;
+                count = j-i+1;
+                while(s[i]==c){
+                    mp[s[i]-'A']--;
+                    i++;
+                    x++;
+                }
+                count-=mp[s[i]-'A'];
+                count -= x;
+                // cnt=0;
+                j++;
+            }
+            
+        }
+        if(ans>=len)return 1;
+        return 0;
+    }
+    int characterReplacement(string s, int k) {
+        int l = 0;
+        int r = s.size();
+        // cout<<r;
+        int ans=0;
+        while(l<=r){
+            int mid = l + (r-l)/2;
+            cout<<mid<<" "<<isCheck(s,mid,k)<<endl;
+            string temp = s;
+            reverse(temp.begin(),temp.end());
+            if(isCheck(s,mid,k) || isCheck(temp,mid,k)){
+                ans = mid;
+                l = mid+1;
+            }
+            else r = mid-1;
+        }
+        // cout<<" "<<isCheck(s,11,k)<<endl;
+        // cout<<s[4]<<" "<<s[14]<<endl;
+        return ans;
+    }
+};
