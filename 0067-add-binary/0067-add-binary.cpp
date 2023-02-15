d87db9b270408f856dd77d25acf262354acdea15
Rupanshu Gupta <72185267+rupanshu1710@users.noreply.github.com>
2023-02-15 15:57:27 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (96.96%) - LeetHub

---
+++ 0067-add-binary/0067-add-binary.cpp
@@ -0,0 +1,84 @@
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        string ans = "";
+        int i=a.size()-1;
+        int j=b.size()-1;
+        int carry=0;
+        while(i>=0 && j>=0){
+            if(a[i]=='1' && b[j]=='1'){
+                if(carry==1){
+                    ans += '1';
+                }
+                else{
+                    ans += '0';
+                    carry = 1;
+                }
+            }
+            else if(a[i]=='0' && b[j]=='0'){
+                if(carry==1){
+                    ans += '1';
+                    carry = 0;
+                }
+                else{
+                    ans += '0';
+                }
+            }
+            else{
+                if(carry==1){
+                    ans += '0';
+                }
+                else{
+                    ans += '1';
+                }
+            }
+            i--;
+            j--;
+        }
+        while(i>=0){
+            if(a[i]=='1'){
+                if(carry==0){
+                    ans += '1';
+                }
+                else{
+                    ans += '0';
+                    carry = 1;
+                }
+            }
+            else{
+                if(carry==0){
+                    ans += '0';
+                }
+                else{
+                    ans += '1';
+                    carry = 0;
+                }
+            }
+            i--;
+        }
+        while(j>=0){
+            if(b[j]=='1'){
+                if(carry==0){
+                    ans += '1';
+                }
+                else{
+                    ans += '0';
+                    carry = 1;
+                }
+            }
+            else{
+                if(carry==0){
+                    ans += '0';
+                }
+                else{
+                    ans += '1';
+                    carry = 0;
+                }
+            }
+            j--;
+        }
+        if(carry)ans += '1';
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
