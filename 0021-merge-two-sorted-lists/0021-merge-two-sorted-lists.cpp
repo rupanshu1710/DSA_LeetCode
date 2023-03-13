2a83f4810cc364711ebe6ab2c4bca56bc8b3dce6
Rupanshu Gupta <72185267+rupanshu1710@users.noreply.github.com>
2023-03-13 22:55:10 +0530

    Time: 4 ms (92.08%), Space: 14.9 MB (49.49%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -10,28 +10,48 @@
  */
 class Solution {
 public:
-    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        ListNode* ans = new ListNode(-1);
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2){
         ListNode* p1 = list1;
         ListNode* p2 = list2;
-        ListNode* p = ans;
-        while(p1 && p2){
-            if(p1->val>p2->val){
-                p->next = p2;
-                p2 = p2->next;
+        if(!p1)return p2;
+        if(!p2)return p1;
+        if(p1->val<p2->val){
+            while(p1->next && p2){
+                if(p1->next->val>p2->val){
+                    ListNode* temp1 = p1->next;
+                    ListNode* temp2 = p2->next;
+                    p1->next = p2;
+                    p2->next = temp1;
+                    p1 = p2;
+                    p2 = temp2;
+                }
+                else{
+                    p1 = p1->next;
+                    // p2 = p2->next;
+                }
             }
-            else{
-                p->next = p1;
-                p1 = p1->next;
+            if(p2)p1->next = p2;
+            return list1;
+        }
+        else{
+            p1 = list2;
+            p2 = list1;
+            while(p1->next && p2){
+                if(p1->next->val>p2->val){
+                    ListNode* temp1 = p1->next;
+                    ListNode* temp2 = p2->next;
+                    p1->next = p2;
+                    p2->next = temp1;
+                    p1 = p2;
+                    p2 = temp2;
+                }
+                else{
+                    p1 = p1->next;
+                    // p2 = p2->next;
+                }
             }
-            p = p->next;
+            if(p2)p1->next = p2;
+            return list2;
         }
-        if(p1){
-            p->next = p1;
-        }
-        if(p2){
-            p->next = p2;
-        }
-        return ans->next;
     }
 };
