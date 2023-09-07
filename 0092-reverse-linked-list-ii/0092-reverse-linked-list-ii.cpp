7ad38364a186de96fa69ee403a2b5a832b1bbab7
Rupanshu Gupta <72185267+rupanshu1710@users.noreply.github.com>
2023-09-07 16:43:47 +0530

    Time: 4 ms (38.20%), Space: 7.4 MB (46.43%) - LeetHub

---
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    ListNode* reverseBetween(ListNode* head, int L, int R) {
+        ListNode*dummy=new ListNode(),*prev=dummy,*left=head,*right=head,*forw=head->next;
+        dummy->next=head;//we have declared dummy variable and initialize its next to head
+		
+        int dif=(R-L);//we have stored the difference between R and L
+        while(--L){//we are traversing to the left node 
+            prev=left;
+            left=forw;
+            right=forw;
+            forw=forw->next;
+        }
+        
+        while(dif--){//after we reach the left node we will start reversing the linked list
+            ListNode*temp=forw->next;
+            forw->next=right;
+            right=forw;
+            forw=temp;
+        }
+        prev->next=right;
+        left->next=forw;
+        return dummy->next;
+    }
+};
