e7aee1c4f01f04d3447bfd2f964fa5870e71792d
Rupanshu Gupta <72185267+rupanshu1710@users.noreply.github.com>
2024-04-27 16:38:08 +0530

    Time: 160 ms (19.34%), Space: 13.7 MB (68.16%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divde-shaw-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence1-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div514-freedom-trail/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divde-shaw-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence1-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div514-freedom-trail.cpp
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divde-shaw-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence1-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div514-freedom-trail/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divde-shaw-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence1-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div514-freedom-trail.cpp
@@ -1,25 +1,44 @@
 class Solution {
 public:
-    int dp[101][101];
-    int solve(int ridx, int kidx, string ring, string key){
-        if(kidx>=key.size())return 0;
-        int ans = INT_MAX;
-        if(dp[ridx][kidx]!=-1)return dp[ridx][kidx];
-        for(int i=0; i<ring.size(); i++){
-            if(ring[i]==key[kidx]){
-                int steps = 1 + min(abs(i-ridx),(int)ring.size()-abs(i-ridx));
-                int totalSteps = steps + solve(i,kidx+1,ring,key);
-                ans = min(ans,totalSteps);
-            }
-        }
-        return dp[ridx][kidx] = ans;
-    }
+    // int dp[101][101];
+    // int solve(int ridx, int kidx, string ring, string key){
+    //     if(kidx>=key.size())return 0;
+    //     if(dp[ridx][kidx]!=-1)return dp[ridx][kidx];
+    //     int ans = INT_MAX;
+    //     for(int i=0; i<ring.size(); i++){
+    //         if(ring[i]==key[kidx]){
+    //             int steps = 1 + min(abs(i-ridx),(int)ring.size()-abs(i-ridx));
+    //             int totalSteps = steps + solve(i,kidx+1,ring,key);
+    //             ans = min(ans,totalSteps);
+    //         }
+    //     }
+    //     return dp[ridx][kidx] = ans;
+    // }
     int findRotateSteps(string ring, string key) {
-        memset(dp,-1,sizeof(dp));
+        // memset(dp,-1,sizeof(dp));
         // vector<int>temp(26,INT_MAX);
         // for(int i=0; i<n; i++){
         //     temp[]
         // }
-        return solve(0,0,ring,key);
+        int n = ring.size();
+        int m = key.size();
+        vector<vector<int>>dp(n+1,vector<int>(m+1,INT_MAX));
+        for(int i=0; i<n; i++){
+            dp[i][m] = 0;
+        }
+        for(int kidx=m-1; kidx>=0; kidx--){
+            for(int ridx=0; ridx<n; ridx++){
+                int ans = INT_MAX;
+                for(int i=0; i<n; i++){
+                    if(ring[i]==key[kidx]){
+                        int steps = 1 + min(abs(i-ridx),n-abs(i-ridx));
+                        int totalSteps = steps + dp[i][kidx+1];
+                        ans = min(ans,totalSteps);
+                    }
+                }
+                dp[ridx][kidx] = ans;
+            }
+        }
+        return dp[0][0];
     }
 };
